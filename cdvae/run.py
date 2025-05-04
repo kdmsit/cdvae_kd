9b8e67f013ed62191a8f5b8f3e7a01ba8ec022da
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-05-05 00:20:36 +0530

    push from local

--- cdvae/run.py
+++ cdvae/run.py
@@ -129,13 +129,13 @@ def run(cfg: DictConfig) -> None:
     (hydra_dir / "hparams.yaml").write_text(yaml_conf)
 
     # Load checkpoint (if exist)
-    ckpts = list(hydra_dir.glob('*.ckpt'))
-    if len(ckpts) > 0:
-        ckpt_epochs = np.array([int(ckpt.parts[-1].split('-')[0].split('=')[1]) for ckpt in ckpts])
-        ckpt = str(ckpts[ckpt_epochs.argsort()[-1]])
-        hydra.utils.log.info(f"found checkpoint: {ckpt}")
-    else:
-        ckpt = None
+    # ckpts = list(hydra_dir.glob('*.ckpt'))
+    # if len(ckpts) > 0:
+    #     ckpt_epochs = np.array([int(ckpt.parts[-1].split('-')[0].split('=')[1]) for ckpt in ckpts])
+    #     ckpt = str(ckpts[ckpt_epochs.argsort()[-1]])
+    #     hydra.utils.log.info(f"found checkpoint: {ckpt}")
+    # else:
+    #     ckpt = None
           
     hydra.utils.log.info("Instantiating the Trainer")
     trainer = pl.Trainer(
