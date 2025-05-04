eb480228a4b8355cca58074379f075eb3b633c94
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-05-05 00:14:46 +0530

    push from local

--- cdvae/run.py
+++ cdvae/run.py
@@ -140,7 +140,7 @@ def run(cfg: DictConfig) -> None:
     hydra.utils.log.info("Instantiating the Trainer")
     trainer = pl.Trainer(
         default_root_dir=hydra_dir,
-        logger=wandb_logger,
+        # logger=wandb_logger,
         callbacks=callbacks,
         deterministic=cfg.train.deterministic,
         check_val_every_n_epoch=cfg.logging.val_check_interval,
