9b58a224f5dd2fb2b25b9a0157a686304ec279db
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-05-05 20:31:23 +0530

    push from local

--- cdvae/eval_utils.py
+++ cdvae/eval_utils.py
@@ -68,7 +68,9 @@ def load_model(model_path, load_data=False, testing=True):
             ckpt_epochs = np.array(
                 [int(ckpt.parts[-1].split('-')[0].split('=')[1]) for ckpt in ckpts])
             ckpt = str(ckpts[ckpt_epochs.argsort()[-1]])
-        model = CDVAE.load_from_checkpoint(ckpt)
+        hparams = os.path.join(model_path, "hparams.yaml")
+        model = CDVAE.load_from_checkpoint(ckpt, hparams_file=hparams, strict=False)
+
         model.lattice_scaler = torch.load(model_path / 'lattice_scaler.pt')
         model.scaler = torch.load(model_path / 'prop_scaler.pt')
 
