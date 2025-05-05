22ba96cce6175f4b020a8ac8bc5a1cff4dfbe050
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-05-05 15:13:40 +0530

    push from local

--- cdvae/pl_modules/gnn.py
+++ cdvae/pl_modules/gnn.py
@@ -274,9 +274,11 @@ class DimeNetPlusPlus(torch.nn.Module):
         adj_t = SparseTensor(row=col, col=row, value=value, sparse_sizes=(num_nodes, num_nodes))
 
         adj_t = adj_t.cpu()
-        # row = row.cpu()
+        row = row.cpu()
         adj_t_row = adj_t[row]
-        
+        adj_t_row.cuda()
+        row.cuda()
+
 
         # adj_t_row = adj_t[row]
 
