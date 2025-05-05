c3b8a438bad31e2a7c34e3e299649e506773cd3e
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-05-05 15:22:00 +0530

    push from local

--- cdvae/pl_modules/gnn.py
+++ cdvae/pl_modules/gnn.py
@@ -378,8 +378,8 @@ class DimeNetPlusPlusWrap(DimeNetPlusPlus):
         offsets = out["offsets"]
 
         j, i = edge_index
-        print(edge_index.device)
-        print(self.triplets.device)
+        # print(edge_index.device)
+        # print(self.triplets.device)
         _, _, idx_i, idx_j, idx_k, idx_kj, idx_ji = self.triplets(edge_index, num_nodes=data.atom_types.size(0))
 
         # Calculate angles.
