5a377cad3e437d59cc64fea97f57f7211d2c3927
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-05-06 14:57:44 +0530

    push from local

--- run_carbon.sh
+++ run_carbon.sh
@@ -8,3 +8,7 @@ CUDA_VISIBLE_DEVICES=1 python3 -W ignore evaluate.py --model_path /home/kishalay
 CUDA_VISIBLE_DEVICES=1 python3 -W ignore evaluate.py --model_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-05/carbon --tasks gen
 
 python3 -W ignore  compute_metrics.py --root_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-05/carbon --tasks gen
+
+
+{'comp_valid': 1.0, 'struct_valid': 0.9999, 'valid': 0.9999, 'wdist_density': 0.1613618060288323, 'wdist_num_elems': 0.0,
+'cov_recall': 0.9990147783251232, 'cov_precision': 0.8205}
--- run_perov.sh
+++ run_perov.sh
@@ -3,6 +3,11 @@ Env- cdvae
 
 CUDA_VISIBLE_DEVICES=0 python3 -W ignore run.py data=perov expname=perov
 
-CUDA_VISIBLE_DEVICES=0 python3 -W ignore evaluate.py --model_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-05/perov --tasks recon
+CUDA_VISIBLE_DEVICES=1 python3 -W ignore evaluate.py --model_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-05/perov --tasks recon
 
 CUDA_VISIBLE_DEVICES=0 python3 -W ignore evaluate.py --model_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-05/perov --tasks gen
+
+python3 -W ignore  compute_metrics.py --root_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-05/perov --tasks gen
+
+{'comp_valid': 0.9843, 'struct_valid': 1.0, 'valid': 0.9843,
+'wdist_density': 0.054827140204242085, 'wdist_num_elems': 0.1009550858652577, 'cov_recall': 0.9955085865257596, 'cov_precision': 0.9846, }
