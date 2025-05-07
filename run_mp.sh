a091adec5394d0f8a8d14fb3ecdc4a83256e7c1c
Kishalay Das <kishalaydas@Kishalays-MacBook-Pro.local>
2025-05-07 18:50:41 +0530

    push from local

--- run_mp.sh
+++ run_mp.sh
@@ -1,13 +1,19 @@
 
-Steps-100
+## Steps - 100
 
 CUDA_VISIBLE_DEVICES=0 python3 -W ignore run.py data=mp_20 expname=mp_20
 
 
-CUDA_VISIBLE_DEVICES=0 python3 -W ignore evaluate.py --model_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-06/mp_20 --tasks recon
+CUDA_VISIBLE_DEVICES=0 python3 -W ignore evaluate.py --model_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-07/mp_20 --tasks recon
 
-CUDA_VISIBLE_DEVICES=0 python3 -W ignore evaluate.py --model_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-06/mp_20 --tasks gen
+CUDA_VISIBLE_DEVICES=0 python3 -W ignore evaluate.py --model_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-07/mp_20 --tasks gen
 
-python3 -W ignore  compute_metrics.py --root_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-06/mp_20 --tasks gen
+python3 -W ignore  compute_metrics.py --root_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-07/mp_20 --tasks gen
 
-{'comp_valid': 0.6264, 'struct_valid': 0.8777, 'valid': 0.5569, 'wdist_density': 0.5882282671255601, 'wdist_num_elems': 0.7203659075834623, 'cov_recall': 0.5337165598054389, 'cov_precision': 0.9942,}
+{'comp_valid': 0.6264, 'struct_valid': 0.8777, 'valid': 0.5569,
+'wdist_density': 0.5882282671255601, 'wdist_num_elems': 0.7203659075834623,
+'cov_recall': 0.5337165598054389, 'cov_precision': 0.9942,}
+
+{'comp_valid': 0.6428, 'struct_valid': 0.8756, 'valid': 0.5728,
+'wdist_density': 0.3723108128578442, 'wdist_num_elems': 0.7853659075834624,
+'cov_recall': 0.4941410568206942, 'cov_precision': 0.9945,}
--- run_perov.sh
+++ run_perov.sh
@@ -18,12 +18,16 @@ python3 -W ignore  compute_metrics.py --root_path /home/kishalay-ng/workplace/cd
 
 CUDA_VISIBLE_DEVICES=0 python3 -W ignore run.py data=perov expname=perov
 
-CUDA_VISIBLE_DEVICES=0 python3 -W ignore evaluate.py --model_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-06/perov --tasks recon
+CUDA_VISIBLE_DEVICES=0 python3 -W ignore evaluate.py --model_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-07/perov --tasks recon
 
-CUDA_VISIBLE_DEVICES=0 python3 -W ignore evaluate.py --model_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-06/perov --tasks gen
+CUDA_VISIBLE_DEVICES=0 python3 -W ignore evaluate.py --model_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-07/perov --tasks gen
 
-python3 -W ignore  compute_metrics.py --root_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-06/perov --tasks gen
+python3 -W ignore  compute_metrics.py --root_path /home/kishalay-ng/workplace/cdvae_kd/singlerun/2025-05-07/perov --tasks gen
 
 {'comp_valid': 0.7824, 'struct_valid': 0.9223, 'valid': 0.7238,
 'wdist_density': 3.148669165338972, 'wdist_num_elems': 2.0120449141347425,
 'cov_recall': 0.0018494055482166448, 'cov_precision': 0.0007}
+
+{'comp_valid': 0.741, 'struct_valid': 0.9242, 'valid': 0.6851,
+'wdist_density': 2.1644098944910226, 'wdist_num_elems': 1.4300449141347422,
+'cov_recall': 0.003170409511228534, 'cov_precision': 0.0021,}
